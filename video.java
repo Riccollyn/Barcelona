75cf012cfdd5c1a2bf322306cc0cb3d84c058d91
Riccollyn <56973314+Riccollyn@users.noreply.github.com>
2019-10-25 15:45:57 +0800

    Add files via upload

---
+++ video.java
@@ -0,0 +1,34 @@
+<html>
+	<head>
+		<title>FCBarcelona</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700%7CPT+Serif:400,700,400italic' rel='stylesheet'>
+		<link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans" rel="stylesheet">
+	</head>
+	
+	<body>
+		<div class="bgimage">
+			<div class="menu">
+				<div class="leftmenu">
+					<h4><a href="index.html"> FCBarcelona </a></h4>
+				</div>
+				
+				<div class="rightmenu">
+					<ul>
+						<li> PLAYER </li>
+						<li><a href="gallery.html"> GALLERY </a></li>
+						<li><a href="video.html"> VIDEO </a></li>
+						<li><a href="trophy.html"> TROPHY </a></li>
+						<li><a href="contact.html"> CONTACT </a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="text">
+				<h2>More than a club</h2>
+				<h1>Més que un club</h1>
+				<button id="buttonone"> LOGIN </button>
+				<button id="buttontwo"> BE A FAN </button>
+			</div>
+		</div>
+	</body>
+</html>
